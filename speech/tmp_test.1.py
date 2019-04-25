a4a1a7d3388e799bce585ddab279c47d6387fd28
Atila Romero <atilaromero@gmail.com>
2019-04-25 10:18:43 -0300

    failling

--- speech/tmp_test.1.py
+++ speech/tmp_test.1.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
 from generateAudioSamples import generateAudioSamples
-import tensorflow.keras.backend as K
 
 last = l0 = tf.keras.layers.Input(shape=(None,1))
 # last = tf.keras.layers.Masking(mask_value=100)(last)
@@ -11,23 +10,18 @@ last = tf.keras.layers.LSTM(128, return_sequences=True, dropout=0.5)(last)
 last = tf.keras.layers.Dense(27)(last)
 last = tf.keras.layers.Activation('softmax')(last)
 
-def ctc_loss(y_true, y_pred):
-    k_labels = y_true
-    k_inputs = y_pred
-    k_input_lens = tf.expand_dims(tf.expand_dims(tf.shape(y_pred)[1],0),0)
-    k_label_lens = tf.expand_dims(tf.expand_dims(tf.shape(y_true)[1],0),0)
-    return K.ctc_batch_cost(k_labels, k_inputs, k_input_lens,
-                              k_label_lens)[0]
+labels = tf.keras.layers.Input(shape=(8,))
+input_length = tf.keras.layers.Input(shape=(1,))
+label_length = tf.keras.layers.Input(shape=(1,))
 
-def test_A():
-    y_true = K.variable(np.asarray([[0,1]]), dtype='int32')
-    y_pred = K.variable(np.asarray(tf.keras.utils.to_categorical([[0,1]])), dtype='float32')
-    cost = K.eval(ctc_loss(y_true,y_pred))
-    print(cost)
-    assert cost[0]**2**0.5 < 1e-5
-test_A()
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    y_pred = y_pred[:, 2:, :]
+    return tf.keras.backend.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
-model = tf.keras.Model([l0], last)
+last = tf.keras.layers.Lambda(ctc_lambda_func, output_shape=(1,))([last, labels, input_length, label_length])
+
+model = tf.keras.Model([l0, labels, input_length, label_length], last)
 # model.summary()
 
 out_chars = 'abcdefghijklmnopqrstuvwxyz '
@@ -37,13 +31,13 @@ assert len(out_chars)==27
 assert chars_to_ix['b'] == 1
 assert ix_to_chars[2] == 'c'
 
-model.compile(loss=ctc_loss,
+model.compile(loss=lambda y_true, y_pred: y_pred,
     optimizer=tf.keras.optimizers.Adam(lr=0.001))
 
 y = 'testando'
-yn = np.asarray([chars_to_ix[i] for i in y])
+yn = np.asarray([chars_to_ix[i] for i in y]).reshape(1,8)
 cat_yn = tf.keras.utils.to_categorical(yn,27)
-print(yn)
-x = np.array(generateAudioSamples(y))
+x = np.array(generateAudioSamples(y)).reshape(1,28224,1)
 print(x.shape)
-# model.fit(x.reshape(1,28224,1), yn.reshape(1,8,27))
+print(yn.shape)
+model.fit([x, yn, np.asarray([[28224]]), np.asarray([[8]])], yn)
