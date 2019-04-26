8cc358579b2b7f804c79f014a7231fd31e060bc2
Atila Romero <atilaromero@gmail.com>
2019-04-26 15:50:46 -0300

    WIP

--- speech/train.py
+++ speech/train.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 from generateAudioSamples import generateAudioSamples
 import tensorflow.keras.backend as K
+import time
+import sys
 
 last = l0 = tf.keras.layers.Input(shape=(None,1))
 # last = tf.keras.layers.Masking(mask_value=100)(last)
@@ -40,11 +42,15 @@ def generate_syllables(n=-1):
     yield c + v
     n-=1
 
+print("creating samples")
+t = time.time()
 xs = [] # array of instances
 ys = []
 max_x = 0 # max of len(x)
 max_y = 0
-for i, word in enumerate(generate_syllables(3)):
+for i, word in enumerate(generate_syllables(100)):
+    print('.',end='')
+    sys.stdout.flush()
     # audio amplitudes
     x = generateAudioSamples(word)
     # first two numbers are parameters for ctc_loss function
@@ -53,7 +59,11 @@ for i, word in enumerate(generate_syllables(3)):
     ys.append(y)
     max_x = max(max_x, len(x))
     max_y = max(max_y, len(y))
+print()
 
+print(time.time() - t)
+t = time.time()
+print("creating matrices")
 # xs has variable size, but must be converted to a np.array
 xarr = np.zeros((len(xs), max_x,1))
 yarr = np.zeros((len(ys), max_y))
@@ -62,7 +72,17 @@ for i,x in enumerate(xs):
 for i,y in enumerate(ys):
     yarr[i,:len(y)] = y
 
+try:
+    model.load_weights('model.h5')
+except OSError:
+    pass
+
+print(time.time() - t)
+t = time.time()
+print("compiling model")
 model.compile(loss=ctc_loss(yarr.shape),
     optimizer=tf.keras.optimizers.SGD(lr=0.001))
 
-model.fit(xarr, yarr)
+for i in range(10):
+    model.fit(xarr, yarr,epochs=10)
+    model.save_weights("model.h5")
