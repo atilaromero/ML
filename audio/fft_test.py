3e6c2e1fbdd58654bd39dcd583393155a474b677
Atila Romero <atilaromero@gmail.com>
2019-05-12 14:46:01 -0300

    useless test

--- audio/fft_test.py
+++
@@ -1,9 +0,0 @@
-from fft import spectrogram_from_file
-
-import matplotlib.pyplot as plt
-def test_original():
-    samples = spectrogram_from_file('dataset/pim.wav')
-    assert samples.shape == (65,221)
-    plt.imshow(samples.T)
-    plt.gca().invert_yaxis()
-    plt.savefig('samples.png')
