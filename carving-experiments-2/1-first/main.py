f1c3f5bc57cf94980c36886709b4379e9b255b74
Atila Romero <atilaromero@gmail.com>
2019-07-06 17:31:04 -0300

    experiments in batch

---
+++ carving-experiments-2/1-first/main.py
@@ -0,0 +1,50 @@
+import sys
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv1D, MaxPooling1D, LSTM, Dense, Activation, TimeDistributed, Flatten
+
+sys.path.append('..')
+from run_experiments import Experiment, run_experiments, save_experiment_results
+
+def D():
+    last = l0 = Input(shape=(512,256))
+    last = Flatten()(last)
+    last = Dense(3)(last)
+    last = Activation('softmax')(last)
+    model = tf.keras.Model([l0], last)
+    myfuncname = sys._getframe().f_code.co_name
+    return Experiment(myfuncname, model, 'first')
+
+def LD():
+    last = l0 = Input(shape=(512,256))
+    last = LSTM(32)(last)
+    last = Dense(3)(last)
+    last = Activation('softmax')(last)
+    model = tf.keras.Model([l0], last)
+    myfuncname = sys._getframe().f_code.co_name
+    return Experiment(myfuncname, model, 'first')
+
+def CL():
+    last = l0 = Input(shape=(512,256))
+    last = Conv1D(3, (32,), strides=16)(last)
+    last = LSTM(3)(last)
+    model = tf.keras.Model([l0], last)
+    myfuncname = sys._getframe().f_code.co_name
+    return Experiment(myfuncname, model, 'first')
+
+experiments = [
+    D(),
+    LD(),
+    CL(),
+]
+
+results = []
+for d in run_experiments(experiments,
+        batch_size=10,
+        validation_batch_size=10,
+        validation_steps=10,
+        steps_per_epoch=10,
+        epochs=2):
+    print(d)
+    results.append(d)
+
+save_experiment_results('experiments.tsv', results)
---
+++ carving-experiments-2/run_experiments.py
@@ -0,0 +1,132 @@
+import os
+import sys
+import time
+import numpy as np
+from collections import namedtuple
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+sys.path.append('../..')
+import utils
+import utils.load
+import utils.sampler
+
+def compile(model):
+    model.compile(loss=tf.keras.losses.categorical_crossentropy,
+        optimizer=tf.keras.optimizers.Adam(),
+        metrics=['accuracy'])
+
+categories = ['pdf','png', 'jpg']
+ix_to_cat = dict([(i,x) for i,x in enumerate(categories)])
+cat_to_ix = dict([(x,i) for i,x in enumerate(categories)])
+
+def sample_sector(path):
+    with open(path, 'rb') as f:
+        f.seek(0,2)
+        size = f.tell()
+        i = np.random.randint(0,size)
+        sector = i//512
+        f.seek(sector*512,0)
+        b = f.read(512)
+        n = np.zeros((512),dtype='int')
+        n[:len(b)] = [int(x) for x in b]
+        return n
+
+def first_sector(path):
+    with open(path, 'rb') as f:
+        b = f.read(512)
+        n = np.zeros((512),dtype='int')
+        n[:len(b)] = [int(x) for x in b]
+        return n
+
+def ys_from_filenames(filenames):
+    cats = [utils.load.category_from_extension(s) for s in filenames]
+    cats = [cat_to_ix[x] for x in cats]
+    ys = utils.one_hot(cats, len(categories))
+    return ys
+
+def xs_from_filenames(filenames, blocks='all'):
+    if blocks=='all':
+        sampler = sample_sector
+    elif blocks == 'first':
+        sampler = first_sector
+    else:
+        raise Exception("blocks must be 'all' or 'first'")
+    xs = np.zeros((len(filenames),512,256))
+    for i,f in enumerate(filenames):
+        x = sampler(f)
+        xs[i] = utils.one_hot(x,256)
+    return xs
+
+def sector_generator(filenames, batch_size, blocks):
+    while True:
+        sample = filenames[:]
+        np.random.shuffle(sample)
+        for i in range(0,len(sample),batch_size):
+            xs = xs_from_filenames(sample[i:i+batch_size], blocks)
+            ys = ys_from_filenames(sample[i:i+batch_size])
+            yield xs, ys
+
+class MyCallback(tf.keras.callbacks.Callback):
+    def __init__(self, save_file=None):
+        self.save_file = save_file
+    def on_epoch_end(self, epoch, logs):
+        if self.save_file:
+            self.model.save(self.save_file)
+        if logs['acc'] > 0.9:
+            self.model.stop_training = True
+
+Experiment = namedtuple('Experiment', 'name model blocks')
+
+def run_experiments(experiments,
+        batch_size,
+        validation_batch_size,
+        validation_steps,
+        steps_per_epoch,
+        epochs):
+    train = utils.load.examples_from('../../datasets/carving/train')
+    validation = utils.load.examples_from('../../datasets/carving/dev')
+    for e in experiments:
+        compile(e.model)
+        start = time.time()
+        history = e.model.fit_generator(sector_generator(train, batch_size, e.blocks),
+            validation_data=sector_generator(validation, validation_batch_size, e.blocks),
+            validation_steps=validation_steps,
+            steps_per_epoch=steps_per_epoch,
+            epochs=epochs,
+            callbacks=[
+                MyCallback(),
+                tf.keras.callbacks.TensorBoard(
+                    log_dir='tboard/' + e.name
+                ),
+            ],
+        )
+        elapsed = time.time() - start
+        trainable_count = int(
+            np.sum([K.count_params(p) for p in set(e.model.trainable_weights)]))
+        epochs_count = len(history.epoch)
+        val_acc = history.history['val_acc'][-1]
+        acc = history.history['acc'][-1]
+        yield {
+            'Name':e.name,
+            'Parameters': trainable_count,
+            'Blocks': e.blocks,
+            'Epochs': epochs_count,
+            'Seconds': elapsed,
+            'Training accuracy': acc,
+            'Validation accuracy': val_acc,
+        }
+
+def save_experiment_results(tsv_path, results):
+    keys = results[0].keys()
+    with open(tsv_path, 'w') as f:
+        f.write('\t'.join(keys))
+        f.write('\n')
+        for r in results:
+            values = [str(r[k]) for k in keys]
+            f.write('\t'.join(values))
+            f.write('\n')
